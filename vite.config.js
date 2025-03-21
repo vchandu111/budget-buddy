58d39c5dc02595b9182ed427ef24cb47450519be
vchandu111 <vchanduu111@gmail.com>
2025-03-21 22:09:29 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
     outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: "assets/[name].[ext]",
+        chunkFileNames: "assets/[name].js",
+        entryFileNames: "assets/[name].js",
       },
     },
   },
